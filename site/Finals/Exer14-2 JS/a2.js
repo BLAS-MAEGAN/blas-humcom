78dd38d62c711d48d11fa0e6f87f3f3822a5002c
BLAS-MAEGAN <20190547@s.ubaguio.edu>
2021-05-10 11:58:50 +0800

    java script

---
+++ site/Finals/Exer14-2 JS/a2.js
@@ -0,0 +1,7 @@
+
+    function myFunction() {
+        document.getElementById("demo").innerHTML = "<p>BLAS, JONAH MAEGAN RACHAEL </p> <p>BACHELOR OF SCIENCE IN INFORMATION AND TECHNOLOGY</p> <p>FIRST YEAR</p> <p>HUMCOM1_IAA1</p>";
+        
+      }
+    
+      
---
+++ site/Finals/Exer14-2 JS/b2.js
@@ -0,0 +1,5 @@
+
+
+ document.write("<p>BLAS, JONAH MAEGAN RACHAEL </p> <p>BACHELOR OF SCIENCE IN INFORMATION AND TECHNOLOGY</p> <p>FIRST YEAR</p> <p>HUMCOM1_IAA1</p>");
+
+    
